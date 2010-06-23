c34408fb9ef81917878614be8f191bd9427d54c8
Daniel Ryan <dryan@dryan.com>
2010-06-23 07:36:13 -0400

    django templatetag

---
+++ twitter_text/templatetags/twitterize.py
@@ -0,0 +1,22 @@
+try:
+    from django.template import Library
+    from django.template.defaultfilters import stringfilter
+except:
+    raise Exception('Django is not installed.')
+
+from twitter_text import TwitterText
+
+register = Library()
+
+@register.filter(name = 'twitter_text')
+@stringfilter
+def twitter_text(text, search_query = False):
+    """
+    Parses a text string through the TwitterText auto_link method and if search_query is passed, through the hit_highlight method.
+    """
+    tt = TwitterText(text)
+    if search_query:
+        tt.highlighter.hit_highlight(search_query)
+    tt.autolink.auto_link()
+    return tt.text
+twitter_text.is_safe = True
