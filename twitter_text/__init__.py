015c7854972686280bdd76917a93c8e1c7a1bfe3
Daniel Ryan <dryan@dryan.com>
2010-06-23 04:04:17 -0400

    update __init__.py to initialize TwitterText object

--- twitter_text/__init__.py
+++ twitter_text/__init__.py
@@ -1,4 +1,15 @@
+from autolink import Autolink
+from extractor import Extractor
+
 class TwitterText(object):
-    def __init__(self):
-        from autolink import Autolink
-        self.autolink = Autolink()
+    def __init__(self, text):
+        self.text = unicode(text) # this will get modified by some functions
+        self.original_text = self.text # this never changes; use it as a fallback or for comparison
+        self.autolink = Autolink(self.text, parent = self)
+        self.extractor = Extractor(self.text)
+        
+    def __unicode__(self):
+        return self.text
+        
+    def __repr__(self):
+        return self.__unicode__()
