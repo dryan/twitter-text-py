b8fa72914db2ee84d764df45b1ec536e6b342ce3
Daniel Ryan <dryan@dryan.com>
2010-06-23 04:00:50 -0400

    change Autolink behavior to require text arg on __init__ and modify TwitterText.text if present

--- twitter_text/autolink.py
+++ twitter_text/autolink.py
@@ -2,7 +2,7 @@ import re
 from regex import REGEXEN
 
 class Autolink(object):
-
+    
     WWW_REGEX = re.compile(r'www\.', re.IGNORECASE)
 
     DEFAULT_URL_CLASS = 'tweet-url'
@@ -15,8 +15,11 @@ class Autolink(object):
 
     HTML_ATTR_NO_FOLLOW = ' rel="nofollow"'
 
-    @staticmethod
-    def auto_link(self, text, **kwargs):
+    def __init__(self, text, **kwargs):
+        self.text = unicode(text)
+        self.parent = kwargs.get('parent', False)
+
+    def auto_link(self, **kwargs):
         """
         Add <a></a> tags around the usernames, lists, hashtags and URLs in the provided text. The
         <a> tags can be controlled with the following kwargs
@@ -32,16 +35,15 @@ class Autolink(object):
         suppress_no_follow::   Do not add rel="nofollow" to auto-linked items
         html_attrs::    A dictionary of HTML attributes to add to non-Twitter URLS
         """
-        return self.auto_link_usernames_or_lists(
-            self.auto_link_urls_custom(
-                self.auto_link_hashtags(text, **kwargs),
-                **kwargs.get('html_attrs', {})
-            ),
-            **kwargs
-        )
+        self.auto_link_urls_custom(**kwargs.get('html_attrs', {}))
+        self.auto_link_hashtags(**kwargs)
+        self.auto_link_usernames_or_lists(**kwargs)
+        if self.parent and hasattr(self.parent, 'text'):
+            self.parent.text = self.text
+
+        return self.text
     
-    @staticmethod
-    def auto_link_usernames_or_lists(self, text, **kwargs):
+    def auto_link_usernames_or_lists(self, **kwargs):
         """
         Add <a></a> tags around the usernames and lists in the provided text. The
         <a> tags can be controlled with the following kwargs
@@ -64,7 +66,7 @@ class Autolink(object):
         kwargs.update(defaults)
         extra_html = kwargs.get('suppress_no_follow', False) or self.HTML_ATTR_NO_FOLLOW
     
-        matches = REGEXEN['auto_link_usernames_or_lists'].finditer(text)
+        matches = REGEXEN['auto_link_usernames_or_lists'].finditer(self.text)
         for match in matches:
             _link = match.group(0)
             if match.group(4) is not None and not kwargs.get('suppress_lists', False):
@@ -77,18 +79,20 @@ class Autolink(object):
                 _username = match.group(3)
                 _link = u'%s<a class="%s" href="%s%s"%s>%s%s</a>' % ( match.group(1), ' '.join( [ kwargs.get('url_class'), kwargs.get('username_class', '') ] ), kwargs.get('username_url_base', ''), _username, extra_html, match.group(2), _username )
                 del(_username)
-            text = text.replace(match.group(0), _link)
+            self.text = self.text.replace(match.group(0), _link)
             del(_link)
+
+        if self.parent and hasattr(self.parent, 'text'):
+            self.parent.text = self.text
 
         del(matches)
         del(extra_html)
         del(kwargs)
         del(defaults)
         
-        return text
+        return self.text
     
-    @staticmethod
-    def auto_link_hashtags(self, text, **kwargs):
+    def auto_link_hashtags(self, **kwargs):
         """
         Add <a></a> tags around the hashtags in the provided text. The
         <a> tags can be controlled with the following kwargs
@@ -106,20 +110,22 @@ class Autolink(object):
         kwargs.update(defaults)
         extra_html = kwargs.get('suppress_no_follow', False) or self.HTML_ATTR_NO_FOLLOW
     
-        matches = REGEXEN['auto_link_hashtags'].finditer(text)
+        matches = REGEXEN['auto_link_hashtags'].finditer(self.text)
         for match in matches:
             _link = u'%s<a href="%s%s" title="#%s" class="%s"%s>%s%s</a>' % ( match.group(1), kwargs.get('hashtag_url_base'), match.group(3), match.group(3), ' '.join( [ kwargs.get('url_class', ''), kwargs.get('hashtag_class', '') ] ), extra_html, match.group(2), match.group(3) )
-            text = text.replace(match.group(0), _link)
+            self.text = self.text.replace(match.group(0), _link)
+
+        if self.parent and hasattr(self.parent, 'text'):
+            self.parent.text = self.text
 
         del(matches)
         del(extra_html)
         del(kwargs)
         del(defaults)
         
-        return text
+        return self.text
     
-    @staticmethod
-    def auto_link_urls_custom(text, **kwargs):
+    def auto_link_urls_custom(self, **kwargs):
         """
         Add <a></a> tags around the URLs in the provided text. Any
         elements in kwargs will be converted to HTML attributes
@@ -141,21 +147,22 @@ class Autolink(object):
         if len(html_attrs):
             html_attrs = ' ' + html_attrs
         
-        matches = REGEXEN['valid_url'].finditer(text)
+        matches = REGEXEN['valid_url'].finditer(self.text)
         for match in matches:
             full_url = match.group(2)
             if match.group(3).find('http') == -1:
                 full_url = u'http://%s' % full_url
-            for i in range(0,7):
-                print match.group(i)
             _link = '%s<a href="%s"%s>%s</a>' % ( match.group(1), match.group(2), html_attrs, match.group(4) )
-            text = text.replace(match.group(0), _link)
+            self.text = self.text.replace(match.group(0), _link)
             del(_link)
             del(full_url)
+
+        if self.parent and hasattr(self.parent, 'text'):
+            self.parent.text = self.text
     
         del(matches)
         del(html_attrs)
         del(kwargs)
         del(defaults)
     
-        return text
+        return self.text
