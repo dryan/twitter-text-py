d3b31e6248506413b2c8d390e9f42af0400f9376
dryan <dryan@dryan.com>
2013-04-25 12:19:40 -0700

    updating version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
  
 setup(
     name='twitter-text-py',
-    version='1.0.4',
+    version='1.0.5',
     description='A library for auto-converting URLs, mentions, hashtags, lists, etc. in Twitter text. Also does tweet validation and search term highlighting.',
     author='Daniel Ryan',
     author_email='dryan@dryan.com',
     url='http://github.com/dryan/twitter-text-py',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
