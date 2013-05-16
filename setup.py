da23b6f51f827c1c790adb7741dcb9c4d2714d17
dryan <dryan@dryan.com>
2013-05-15 21:36:15 -0400

    change setup.py version to 2.0.0 and mark as stable

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
  
 setup(
     name='twitter-text-py',
-    version='1.0.5',
+    version='2.0.0',
     description='A library for auto-converting URLs, mentions, hashtags, lists, etc. in Twitter text. Also does tweet validation and search term highlighting.',
     author='Daniel Ryan',
     author_email='dryan@dryan.com',
     url='http://github.com/dryan/twitter-text-py',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
